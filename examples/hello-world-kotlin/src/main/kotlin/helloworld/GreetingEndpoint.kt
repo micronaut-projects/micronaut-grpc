23d464bbeea584fd00a3553c395d0792848c156f
Steve Schneider <45122709+schneidersteve@users.noreply.github.com>
2020-05-20 12:11:21 +0200

    Migrate example to the official gRPC Kotlin/JVM implementation (#117)

--- examples/hello-world-kotlin/src/main/kotlin/helloworld/GreetingEndpoint.kt
+++ examples/hello-world-kotlin/src/main/kotlin/helloworld/GreetingEndpoint.kt
@@ -1,19 +1,18 @@
 package helloworld
 
 // tag::imports[]
-import io.grpc.stub.StreamObserver
 import javax.inject.Singleton
 // end::imports[]
 
 // tag::clazz[]
 @Singleton // <1>
-class GreetingEndpoint(val greetingService : GreetingService) : GreeterGrpc.GreeterImplBase() { // <2>
-    override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
+@Suppress("unused")
+class GreetingEndpoint(val greetingService : GreetingService) : GreeterGrpcKt.GreeterCoroutineImplBase() { // <2>
+    override suspend fun sayHello(request: HelloRequest): HelloReply {
     	// <3>
         val message = greetingService.sayHello(request.name)
         val reply = HelloReply.newBuilder().setMessage(message).build()
-        responseObserver.onNext(reply)
-        responseObserver.onCompleted()
+        return reply
     }
 }
 // end::clazz[]
