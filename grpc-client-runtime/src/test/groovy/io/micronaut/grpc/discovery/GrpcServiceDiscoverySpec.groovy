6cb776b6b0eb020934a9501d5b1dc3510ca2d918
graemerocher <graeme.rocher@gmail.com>
2021-02-12 11:38:45 +0100

    Remove gRPC converter, use resolved addresses and remove deprecated features. Fixes #270

--- grpc-client-runtime/src/test/groovy/io/micronaut/grpc/discovery/GrpcServiceDiscoverySpec.groovy
+++ grpc-client-runtime/src/test/groovy/io/micronaut/grpc/discovery/GrpcServiceDiscoverySpec.groovy
@@ -39,7 +39,7 @@ class GrpcServiceDiscoverySpec extends Specification {
 
         and:'then a client is run that declares the service'
         ApplicationContext client = ApplicationContext.run([
-                (GrpcNameResolverFactory.ENABLED): true,
+                (GrpcNameResolverProvider.ENABLED): true,
                 'grpc.channels.greet.plaintext':true,
                 'micronaut.http.services.greet.url': server.URL.toString()
         ])
@@ -63,7 +63,7 @@ class GrpcServiceDiscoverySpec extends Specification {
 
         and:'then a client is run that declares the service'
         ApplicationContext client = ApplicationContext.run([
-                (GrpcNameResolverFactory.ENABLED): true,
+                (GrpcNameResolverProvider.ENABLED): true,
                 'grpc.client.plaintext':true,
                 'my.port':server.getPort()
         ])
