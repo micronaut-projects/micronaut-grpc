814bfe18275bc66869956bd3c76edc67d6791185
Tim Yates <tim.yates@gmail.com>
2022-05-11 11:19:52 +0100

    Add tests

---
+++ test-suite-java/src/test/java/helloworld/HealthCheckTest.java
@@ -0,0 +1,49 @@
+package helloworld;
+
+import io.grpc.ManagedChannel;
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
+import io.grpc.protobuf.services.HealthStatusManager;
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.grpc.annotation.GrpcChannel;
+import io.micronaut.grpc.server.GrpcServerChannel;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+class HealthCheckTest {
+
+    @Inject
+    HealthGrpc.HealthBlockingStub healthStub;
+
+    @Inject
+    HealthService healthService;
+
+    @Test
+    void testHealth() {
+        assertEquals(
+                HealthCheckResponse.ServingStatus.SERVING,
+                healthStub.check(HealthCheckRequest.newBuilder().build()).getStatus()
+        );
+
+        healthService.setStatus(HealthStatusManager.SERVICE_NAME_ALL_SERVICES, HealthCheckResponse.ServingStatus.NOT_SERVING);
+
+        assertEquals(
+                HealthCheckResponse.ServingStatus.NOT_SERVING,
+                healthStub.check(HealthCheckRequest.newBuilder().build()).getStatus()
+        );
+    }
+
+    @Factory
+    static class HealthClients {
+        @Bean
+        HealthGrpc.HealthBlockingStub blockingStub(@GrpcChannel(GrpcServerChannel.NAME) ManagedChannel channel) {
+            return HealthGrpc.newBlockingStub(channel);
+        }
+    }
+}
