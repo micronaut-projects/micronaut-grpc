1326703e24a4a5ec04058ee12d2d77177cb94473
krickert <krickert@gmail.com>
2025-03-01 19:47:24 -0500

    Documentation, clean up, segmenting logic, cleaning build files, created sample project for tutorial.

--- test-suite-protobuff-json-java/src/test/groovy/com/example/services/GreeterServiceSpec.groovy
+++ test-suite-protobuff-json-java/src/test/groovy/com/example/services/GreeterServiceSpec.groovy
@@ -3,34 +3,40 @@ package com.example.services
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.micronaut.context.annotation.Property
+import io.micronaut.grpc.annotation.GrpcChannel
+import io.micronaut.grpc.server.GrpcServerChannel
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.example.grpc.GreeterGrpc
 import org.example.grpc.HelloRequest
+import spock.lang.Shared
 import spock.lang.Specification
 
 @MicronautTest
 @Property(name = "grpc.server.port", value = "{random.port}") // Random port
 class GreeterServiceSpec extends Specification {
 
+    @GrpcChannel(GrpcServerChannel.NAME)
     @Inject
     ManagedChannel channel
 
+    @Shared
+    GreeterGrpc.GreeterBlockingStub blockingStub
+
+    void setup() {
+        blockingStub = GreeterGrpc.newBlockingStub(channel)
+    }
+
     void "test sayHello returns correct greeting"() {
-        given: "a gRPC stub"
-        def stub = GreeterGrpc.newBlockingStub(channel)
+        when:
+        def response = blockingStub.sayHello(
+                HelloRequest.newBuilder().setName("John").build()
+        )
 
-        and: "a request with a name"
-        def request = HelloRequest.newBuilder()
-                .setName("John")
-                .build()
-
-        when: "we call the service"
-        def response = stub.sayHello(request)
-
-        then: "we get the expected greeting"
+        then:
         response.greeting == "Hello, John"
     }
+
 
     void cleanup() {
         channel?.shutdown()
